a0959a9c9a01f10b14593f9c71ce8724f14bdd38
Paul.Jones <paul.jones@creditplus.co.uk>
2020-04-07 22:00:27 +0100

    wip (webcrypto rollup issues...)

---
+++ rollup.browser.config.js
@@ -0,0 +1,26 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+// UMD build (for browsers)
+export default {
+  input: 'src/index.ts',
+  output: {
+    name: 'ne_crypto',
+    file: pkg.browser,
+    format: 'umd',
+    globals: {
+      '@ne1410s/text': 'ne_text'
+    }
+  },
+  plugins: [
+    resolve(), // find external modules
+    commonjs(), // convert external modules to ES modules
+    typescript(),
+    terser({
+      include: '*.umd.min.js'
+    }),
+  ]
+};
